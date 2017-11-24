772891c3d3245fa254747c63cfcd158d25870073
usuario <usuario@10.22.148.63>
2017-11-24 09:25:56 +0000

    version 2 Kata5p2

--- src/kata5P2/main/Kata5P2.java
+++ src/kata5P2/main/Kata5P2.java
@@ -1,15 +1,17 @@
 package kata5P2.main;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.List;
 import kata5P2.view.HistogramDisplay;
 import kata5P2.model.Histogram;
 import kata5P2.view.MailHistogramBuilder;
 import kata5P2.view.MailListBuilder;
+import kata5P2.view.MailListReaderDDBB;
 
 public class Kata5P2 {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException {
         
         Kata5P2 kata = new Kata5P2();
         kata.execute();
@@ -18,15 +20,15 @@ public class Kata5P2 {
     private List<String> mailList;
     private Histogram<String> histogram;
     
-    private void execute() throws IOException {
+    private void execute() throws IOException, ClassNotFoundException, SQLException {
         input();
         process();
         output();
     }
     
-    private void input() throws IOException {
-        String filename = "C:\\Users\\usuario\\Documents\\NetBeansProjects\\Kata4\\src\\emails.txt";
-        mailList = MailListBuilder.read(filename);
+    private void input() throws IOException, ClassNotFoundException, SQLException {
+        //String filename = "C:\\Users\\usuario\\Documents\\NetBeansProjects\\Kata4\\src\\emails.txt";
+        mailList = MailListReaderDDBB.read();
     }
     
     private void process() {
